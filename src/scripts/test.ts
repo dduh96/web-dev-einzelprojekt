a197b6cf83b6e30d679a83934f6ac8d03e1ff4eb
daniela duh <daniela.duh@gmail.com>
2022-09-13 18:27:02 +0200

    init repo

---
+++ src/scripts/test.ts
@@ -0,0 +1,8 @@
+// test.ts
+export function printMessage(msg: string | string[]) {
+    if (typeof msg === "string") {
+        console.log(`welcome ${msg}`);
+    } else {
+        console.log(`welcome ${msg.join()}`);
+    }
+}
