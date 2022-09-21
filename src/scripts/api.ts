7bfe293e634462dedf407b00b037b282c795a142
daniela duh <daniela.duh@gmail.com>
2022-09-21 23:00:12 +0200

    consume and render api

--- src/scripts/api.ts
+++ src/scripts/api.ts
@@ -1,2 +1,20 @@
 const getUrl = 'https://api.britney.rest/?format=text';
 
+const btnGenerate = document.getElementById('btn-generate');
+btnGenerate.onclick = function sendQuoteRequest() {
+    const xhttps = new XMLHttpRequest();
+    xhttps.onload = function onResponseLoaded() {
+        const headlineQuote = document.createElement('h2');
+        headlineQuote.innerText = this.responseText;
+        headlineQuote.classList.add('text-center');
+        const divApi = document.getElementById('div-quote');
+
+        if (divApi.childElementCount >= 3) {
+            divApi.innerHTML = '';
+        }
+        divApi.append(headlineQuote); // todo replace instead of add
+    };
+
+    xhttps.open('GET', getUrl);
+    xhttps.send();
+};
